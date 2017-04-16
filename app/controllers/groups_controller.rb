9b3f6455ba0c9491bdf872248466cff7a681b094
songhao <songhao@songhaodeMacBook-Pro.local>
2017-04-16 12:31:08 +0800

    user before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
 
   before_action :authenticate_user!, only: [:show, :new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -26,19 +27,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "你没有权限"
-    end
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "你没有权限"
-    end
 
     if @group.update(group_params)
       redirect_to groups_path, :notice => "修改成功"
@@ -48,12 +39,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "你没有权限"
-    end
-
     @group.destroy
     redirect_to groups_path, :alert => "删除成功"
   end
@@ -62,6 +47,14 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:title, :description)
+  end
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "你没有权限"
+    end
   end
 
 end
