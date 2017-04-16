2060fedb626eff486254abbe92ba9333d5801d14
songhao <songhao@songhaodeMacBook-Pro.local>
2017-04-16 16:33:29 +0800

    add something

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "修改成功"
+    else
+      render :edit
+  end
+end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "删除成功"
+  end
+
 
   private
 
